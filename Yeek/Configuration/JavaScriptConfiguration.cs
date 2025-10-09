ca98812442d834916fe0c50944af6f3da91da0c6
Simon <63975668+Simyon264@users.noreply.github.com>
2025-10-10 01:26:09 +0200

    edit default requirement

--- Yeek/Configuration/JavaScriptConfiguration.cs
+++ Yeek/Configuration/JavaScriptConfiguration.cs
@@ -7,7 +7,7 @@ public class JavaScriptConfiguration
     public const string Name = "JavaScript";
 
     public bool Enable { get; set; } = true;
-    public TrustLevel RequiredTrust { get; set; } = TrustLevel.Moderator;
+    public TrustLevel RequiredTrust { get; set; } = TrustLevel.Trusted;
 
     /// <summary>
     /// Allowed characters for a js script.
